de7db8c5c2e67abbe95cc5f094b4348a2c427806
Michael Baranes <info@av.co.il>
2026-07-01 15:19:28 +0300

    Rename stat label: כניסות → אירועי כניסה for clarity

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'club-v25';
+const CACHE_NAME = 'club-v26';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './user.html', './register.html', './jsQR.js', './qrcode.min.js', './manifest.json'];
 
 self.addEventListener('install', e => {
