d735c8ccb198e45f9dee801a35d7f0126a9e7507
Michael Baranes <info@av.co.il>
2026-07-18 08:55:20 +0300

    Add back button to member card page for when NFC misfires open it directly

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'club-v105';
+const CACHE_NAME = 'club-v106';
 const NEVER_CACHE = ['app.js', 'index.html'];
 const ASSETS = ['./style.css', './user.html', './register.html', './guest.html', './jsQR.js', './qrcode.min.js', './manifest.json'];
 
